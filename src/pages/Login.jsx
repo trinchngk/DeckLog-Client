d9296a2c20540d440ea69439b78a7d6caf41d770
trinchngk <trin.pasith@gmail.com>
2025-03-02 01:15:17 -0500

    login fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,24 +1,28 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useSnackbar } from 'notistack';
 import { auth, provider } from '../firebase';
 import { signInWithPopup } from 'firebase/auth';
 import { useDispatch } from "react-redux";
 import { loginStart, loginFailure, loginSuccess } from '../redux/userSlice';
 import { FcGoogle } from "react-icons/fc";
 
-
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [username, setUsername] = useState('');
   const [error, setError] = useState(null);
+  const [isSignup, setIsSignup] = useState(false);
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const { enqueueSnackbar } = useSnackbar();
 
   const handleLogin = async (e) => {
     e.preventDefault();
     dispatch(loginStart());
-    setError(null); // Clear any existing error
+    setError(null);
 
     try {
       const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, 
@@ -29,8 +33,8 @@ const Login = () => {
           'Accept': 'application/json',
         }});
       dispatch(loginSuccess(response.data));
-      console.log('Login successful:', response.data);
-      console.log('Login response headers:', response.headers);
+      // console.log('Login successful:', response.data);
+      // console.log('Login response headers:', response.headers);
       setTimeout(() => {
         window.location.href = '/';
       }, 100);
@@ -40,6 +44,22 @@ const Login = () => {
       console.error('Login error:', err);
       console.error('Login error details:', err.response);
       setError(err.response?.data?.message || 'Login failed. Please try again.');
+    }
+  };
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setError(null);
+
+    try {
+      const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, { username, email, password }, { withCredentials: true });
+      enqueueSnackbar('Signup successful', { variant: 'success' });
+      // console.log('Signup successful:', response.data);
+      navigate('/login');
+
+    } catch (err) {
+      console.error('Signup error:', err);
+      setError(err.response?.data?.message || 'Signup failed. Please try again.');
     }
   };
 
@@ -70,15 +90,31 @@ const Login = () => {
   };
 
   return (
-    <div className="font-sans flex flex-col items-center justify-center h-screen bg-[#201c1c]">
-      <div className="w-full max-w-sm bg-[#2E2E33] shadow-md rounded-md p-6">
-        <h1 className="text-2xl font-semibold text-center text-white mb-6">Login</h1>
+    <div className="font-sans flex flex-row items-center justify-start h-screen bg-[#201c1c] gap-10 p-12">
+      <div className="w-1/3 max-w-md bg-[#2E2E33] shadow-md rounded-lg border-2 border-blue-500 p-6 ml-12 py-10">
+        <h1 className="text-2xl font-semibold text-center text-white mb-10">{isSignup ? 'Signup to MoveMonkey' : 'Login to MoveMonkey'}</h1>
         {error && (
           <div className="bg-red-100 text-red-700 text-sm p-2 rounded mb-4">
             {error}
           </div>
         )}
-        <form onSubmit={handleLogin}>
+        <form onSubmit={isSignup ? handleSignup : handleLogin}>
+          {isSignup && (
+            <div className="mb-4">
+              <label htmlFor="username" className="block text-sm font-medium text-white">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                placeholder="Enter your username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="my-2 bg-[#2E2E33] border-2 border-blue-500 px-4 py-2 w-full text-white rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
+                required
+              />
+            </div>
+          )}
           <div className="mb-4">
             <label htmlFor="email" className="block text-sm font-medium text-white">
               Email
@@ -89,7 +125,7 @@ const Login = () => {
               placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full mt-1 p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+              className="my-2 bg-[#2E2E33] border-2 border-blue-500 px-4 py-2 w-full text-white rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
               required
             />
           </div>
@@ -103,34 +139,33 @@ const Login = () => {
               placeholder="Enter your password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full mt-1 p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-200"
+              className="my-2 bg-[#2E2E33] border-2 border-blue-500 px-4 py-2 w-full text-white rounded-lg focus:outline-none focus:ring focus:ring-blue-200"
               required
             />
           </div>
           <button
             type="submit"
-            className="mt-1 w-full bg-blue-500 text-white p-2 rounded hover:bg-white hover:text-blue-500 focus:outline-none focus:ring focus:ring-blue-300"
+            className="mt-1 w-full bg-blue-500 border-2 border-blue-500 text-white p-2 rounded-lg hover:bg-white hover:text-blue-500 focus:outline-none focus:ring focus:ring-blue-300"
           >
-            Login
+            {isSignup ? 'Signup' : 'Login'}
           </button>
-          
         </form>
-        <div className="mt-4 flex justify-center">
-          <button className="text-center bg-white text-[#2E2E33] px-3 py-2 rounded hover:bg-gray-700 hover:text-white focus:outline-none focus:ring focus:ring-blue-300 flex" 
-            onClick={signInWithGoogle}>
-              <FcGoogle className='mt-1 mr-2'/>            
-              Login with Google
-
-          </button>          
+        <button className="mt-3 w-full justify-center text-center bg-white border-2 border-blue-500 text-[#2E2E33] px-3 py-2 rounded-lg hover:bg-[#2E2E33] hover:text-blue-500 focus:outline-none focus:ring focus:ring-blue-300 flex" 
+          onClick={signInWithGoogle}>
+            <FcGoogle className='mt-1 mr-2'/>            
+            Login with Google
+        </button>          
+        <div className="mt-10 text-md text-center text-gray-500 flex  flex-col">
+          {isSignup ? 'Already have an account? ' : 'Don\'t have an account? '}
+          <button onClick={() => setIsSignup(!isSignup)} className="mt-3 w-full justify-center text-center bg-[#2E2E33] border-2 border-blue-500 text-white px-3 py-2 rounded-lg hover:bg-blue-500 hover:text-[#2E2E33] focus:outline-none focus:ring focus:ring-blue-300 flex">
+            {isSignup ? 'Login' : 'Sign up'}
+          </button>
         </div>
-
-        
-        <div className="mt-4 text-sm text-center text-gray-500">
-          Don't have an account?{' '}
-          <a href="/signup" className="text-blue-500 hover:underline">
-            Sign up
-          </a>
-        </div>
+      </div>
+      <div className="w-2/3 m-6 max-w-sm shadow-md rounded-md p-6">
+        <h1 className="text-blue-500 text-9xl font-mono">
+          MoveMonkey          
+        </h1>
       </div>
     </div>    
   );
